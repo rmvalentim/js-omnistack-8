e90f19a45eed368599b699db92a44c5ed88660ba
Rafael Valentim <rmoraesvalentim@gmail.com>
2019-08-07 09:08:46 -0300

    Corrigindo gitignore

--- backend/src/credentials/mongoDbConfig.js
+++
@@ -1,6 +0,0 @@
-const username = 'rafael';
-const password = 'cOLwFireCbHIB71r';
-const databaseName = 'omnistack';
-
-module.exports =  
-`mongodb+srv://${username}:${password}@cluster0-rrrjc.mongodb.net/${databaseName}?retryWrites=true&w=majority`
